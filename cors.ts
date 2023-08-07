f81bd13c8fda108d2980a404db12307e2f44743d
Fakhry NM <fakhry@example.com>
2023-08-07 23:27:20 +0700

    update

--- cors.ts
+++ cors.ts
@@ -1,7 +1,7 @@
 // cors.ts
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://datatrainigsite.streamlit.app', // Replace '*' with the specific origin you want to allow
+  'Access-Control-Allow-Origin': '*', // Replace '*' with the specific origin you want to allow
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
 };
